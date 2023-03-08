cabbdc5445a241a635d9066f2f0ee8094df53a1e
noveky <65780673+noveky@users.noreply.github.com>
2023-03-08 16:11:43 +0800

    Update Program.cs

--- Assignment2/Assignment2/Program.cs
+++ Assignment2/Assignment2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.Intrinsics.X86;
+using System.Text;
 
 namespace Assignment2
 {
@@ -7,17 +8,21 @@ namespace Assignment2
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("------- Assignment 2 -------");
-			Console.WriteLine("1. Prime factors");
-			Console.WriteLine("2. Array max, min, avg, sum");
-			Console.WriteLine("3. Sieve of Eratosthenes");
-			Console.WriteLine("4. Toeplitz matrix");
-			Console.WriteLine("----------------------------");
 
 			while (true)
 			{
 				try
 				{
+					// 模块说明
+					Console.Clear();
+					Console.WriteLine("------- Assignment 2 -------");
+					Console.WriteLine("1. Prime factors");
+					Console.WriteLine("2. Array max, min, avg, sum");
+					Console.WriteLine("3. Sieve of Eratosthenes");
+					Console.WriteLine("4. Toeplitz matrix");
+					Console.WriteLine("----------------------------");
+
+					// 选择模块输入
 					string? strRead;
 					int ent = -1;
 					Console.Write("Choose a module (1~4) or enter 0 to exit: ");
@@ -29,20 +34,28 @@ namespace Assignment2
 						case 0: return;
 						case 1:
 							{
+								// 输入
 								int num = -1;
 								Console.Write("Enter a positive integer: ");
 								strRead = Console.ReadLine();
 								if (strRead != null) num = int.Parse(strRead);
+
+								// 解题
 								List<int> facs = Problem1(num);
+
+								// 输出
+								StringBuilder stbWrite = new StringBuilder($"{num} = ");
 								foreach (var fac in facs)
 								{
-									Console.Write($"{fac} ");
+									stbWrite.Append($"{fac} * ");
 								}
-								Console.WriteLine();
+								stbWrite.Remove(stbWrite.Length - 3, 3);
+								Console.WriteLine(stbWrite);
 							}
 							break;
 						case 2:
 							{
+								// 输入
 								Console.Write("Enter an integer array: ");
 								strRead = Console.ReadLine();
 								string[] strReads = Array.Empty<string>();
@@ -54,31 +67,42 @@ namespace Assignment2
 									int ai = int.Parse(strReads[i]);
 									arr[i] = ai;
 								}
+
+								// 解题
 								List<object> result = Problem2(arr);
+
+								// 输出
 								Console.WriteLine($"Max: {result[0]} Min: {result[1]} Avg: {result[2]} Sum: {result[3]}");
 							}
 							break;
 						case 3:
 							{
+								// 初始化
 								int num = 100;
 								int[] arr = new int[num + 1]; // 等于0表示被筛去
+
+								// 解题
 								Problem3(arr);
-								Console.Write("Primes in range 2~100: [");
+
+								// 输出
+								StringBuilder stbWrite = new StringBuilder("Primes in range 2~100: [");
 								foreach (var i in arr)
 								{
-									if (i != 0) Console.Write($"{i} ");
+									if (i != 0) stbWrite.Append($"{i} ");
 								}
-								Console.WriteLine("]");
+								stbWrite[stbWrite.Length - 1] = ']';
+								Console.WriteLine(stbWrite);
 							}
 							break;
 						case 4:
 							{
+								// 输入
 								int M = -1, N = 0;
 								Console.Write("Enter M: ");
 								strRead = Console.ReadLine();
 								if (strRead != null) M = int.Parse(strRead);
 								if (M < 0) throw new Exception();
-								Console.WriteLine("Enter a M*N matrix: ");
+								Console.WriteLine("Enter a M * N matrix: ");
 								List<int>[] rows = new List<int>[M];
 								for (int i = 0; i < M; ++i)
 								{
@@ -104,7 +128,11 @@ namespace Assignment2
 										matrix[i, j] = rows[i][j];
 									}
 								}
+
+								// 解题
 								bool isToeplitz = Problem4(matrix);
+
+								// 输出
 								Console.WriteLine($"Is Toeplitz matrix: {isToeplitz}");
 							}
 							break;
@@ -115,7 +143,11 @@ namespace Assignment2
 				{
 					Console.WriteLine("Invalid input!");
 				}
+
+				// 暂停
 				Console.WriteLine();
+				Console.Write("Press any key to restart...");
+				Console.ReadKey();
 			}
 		}
 
@@ -123,15 +155,15 @@ namespace Assignment2
 		{
 			if (num <= 0) throw new Exception();
 			List<int> facs = new List<int>();
-			for (int i = 2; i <= num; ++i)
+			for (int i = 2; i * i <= num; ++i)
 			{
-				if (num % i == 0)
+				while (num % i == 0)
 				{
 					facs.Add(i);
 					num /= i;
 				}
-				while (num % i == 0) num /= i;
 			}
+			if (num != 1) facs.Add(num);
 			return facs;
 		}
 
